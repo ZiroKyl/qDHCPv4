cbd925cb2ed9e2f71fc84aab24d25528b4459e94
ZiroKyl <searchcool@yandex.ru>
2014-11-29 15:42:24 +0600

    Refactoring #1 End

--- qDHCPv4.go
+++ qDHCPv4.go
@@ -127,7 +127,7 @@ func main() {
 
 
 	dhcp.ListenAndServe(func() dhcp.Handler {
-		//all IP - array + linked queue for last free
+		//all leases - array + linked queue for last free
 		//MAC - map
 
 		type MAC48 [6]byte;
@@ -140,153 +140,154 @@ func main() {
 			SERVER_IP = net.IP{194,188,64,28};
 		)
 
-		type Stage byte;
+		var zeroIP  = net.IP{0,0,0,0};
+		var zeroMAC = MAC48{0,0,0,0,0,0};
+
+		type IPStage byte;
 		const (
-			IP_Free     Stage = iota;
+			IP_Removed IPStage = iota;
+			IP_Free;
 			IP_Reserved;
-			IP_NotFree;
+			IP_Issued;
 		)
 
-		type queueIP struct{
-			ip            net.IP;
-			 mac	    MAC48;
-			 //expiry	time.Time;
-			stage         Stage;
-			nextFreeIP   *queueIP;
-			beforeFreeIP *queueIP;
+		type linkedLease struct{
+			stage   IPStage;
+			ip      net.IP;
+			mac	    MAC48;
+			pervL  *linkedLease;
+			nextL  *linkedLease;
 		};
 
-		var IP[IP_Range]  queueIP;
-		var lastFreeIP   *queueIP;
-		var endFreeIP    *queueIP;
-		var endReserveIP *queueIP;
+		var leases[IP_Range]  linkedLease;
+		var qLastFreeL       *linkedLease; //    Queue-->
+		var qFirstFreeL      *linkedLease; // -->Queue
+		var firstReserveL    *linkedLease;
 
-		{//Init IP
-			IP[len(IP)-1].nextFreeIP = nil;
-			IP[len(IP)-1].beforeFreeIP = &IP[len(IP)-2];
-			IP[len(IP)-1].stage = IP_Free;
-			IP[len(IP)-1].ip = dhcp.IPAdd(START_IP, len(IP)-1);
+		{//Init leases
+			leases[len(leases)-1].pervL =  nil;
+			leases[len(leases)-1].nextL = &leases[len(leases)-2];
+			leases[len(leases)-1].stage =  IP_Free;
+			leases[len(leases)-1].ip    =  dhcp.IPAdd(START_IP, len(leases)-1);
 
-			for i := len(IP) - 2; i > 0; i-- {
-				IP[i].nextFreeIP = &IP[i+1];
-				IP[i].beforeFreeIP = &IP[i-1];
-				IP[i].stage = IP_Free;
-				IP[i].ip = dhcp.IPAdd(START_IP, i);
+			for i:=len(leases)-2; i>0; i-- {
+				leases[i].pervL = &leases[i+1];
+				leases[i].nextL = &leases[i-1];
+				leases[i].stage =  IP_Free;
+				leases[i].ip    =  dhcp.IPAdd(START_IP, i);
 			}
 
-			IP[0].nextFreeIP = &IP[1];
-			IP[0].beforeFreeIP = nil;
-			IP[0].stage = IP_Free;
-			IP[0].ip = START_IP;
+			leases[0].pervL = &leases[1];
+			leases[0].nextL =  nil;
+			leases[0].stage =  IP_Free;
+			leases[0].ip    =  START_IP;
 
-			lastFreeIP = &IP[0];
-			endFreeIP = &IP[len(IP)-1];
-			endReserveIP = nil;
-		}//Init IP end
+			qLastFreeL    = &leases[0];
+			qFirstFreeL   = &leases[len(leases)-1];
+			firstReserveL =  nil;
+		}//Init leases end
 
-		var offsetToIP = func(offset int) *queueIP{
-			return &IP[offset];
+		var Lease = func(offset int) *linkedLease{
+			return &leases[offset];
 		};
-		var getLastFreeIP = func() (freeIP *queueIP){
-			{//FreeIP
-				if lastFreeIP == nil { return nil; }
+		var removeLease = func(lease/*!nil*/ *linkedLease, firstLease, lastLease **linkedLease/**(nil),!=*/){
+			lease.stage = IP_Removed;
 
-				if lastFreeIP.nextFreeIP != nil { lastFreeIP.nextFreeIP.beforeFreeIP = nil; }
+			if lease.nextL != nil { lease.nextL.pervL = lease.pervL; }
+			if lease.pervL != nil { lease.pervL.nextL = lease.nextL; }
 
-				freeIP = lastFreeIP;
-				lastFreeIP = lastFreeIP.nextFreeIP;
+			if lease == *firstLease { *firstLease  = lease.nextL; }
+			if lease == *lastLease  { *lastLease   = lease.pervL; }
 
-				if lastFreeIP == nil { endFreeIP = nil; }
+			lease.pervL = nil;
+			lease.nextL = nil;
+		};
+		var putLease = func(lease/*!nil*/ *linkedLease, firstLease, lastLease **linkedLease/**(!nil),==*/, stage IPStage){
+			lease.pervL = nil;
+			lease.nextL = *firstLease;
 
-				freeIP.nextFreeIP = nil;
-			}
-			{//ReserveIP
-				freeIP.nextFreeIP = nil;
-				freeIP.beforeFreeIP = endReserveIP;
+			if *firstLease != nil { (*firstLease).pervL = lease;
+			}else{ *lastLease = lease; }
 
-				if endReserveIP != nil { endReserveIP.nextFreeIP = freeIP; }
-
-				endReserveIP = freeIP;
-				freeIP.stage = IP_Reserved;
-			}
+			*firstLease = lease;
+			lease.stage = stage;
+		};
+		var GetFreeL = func(reqL/*!nil*/ *linkedLease) (freeL *linkedLease){
+			removeLease(reqL, &qFirstFreeL, &qLastFreeL);
+			freeL = reqL;
+			putLease(freeL, &firstReserveL, &firstReserveL, IP_Reserved);
 
 			return;
 		};
-		var getFreeIP = func(reqIP *queueIP) (freeIP *queueIP){
-			{//FreeIP
-				if reqIP.beforeFreeIP != nil { reqIP.beforeFreeIP.nextFreeIP = reqIP.nextFreeIP; }
-				if reqIP.nextFreeIP   != nil { reqIP.nextFreeIP.beforeFreeIP = reqIP.beforeFreeIP; }
+		var LastFreeL = func() (freeL *linkedLease){
+			if qLastFreeL == nil { return nil; }
 
-				if reqIP == endFreeIP  { endFreeIP  = reqIP.beforeFreeIP; }
-				if reqIP == lastFreeIP { lastFreeIP = reqIP.nextFreeIP; }
+			freeL = GetFreeL(qLastFreeL);
+			return;
+		};
+		var GetReserveL = func(resL/*!nil*/ *linkedLease) (issuedL *linkedLease){
+			var nilL *linkedLease;
 
-				reqIP.nextFreeIP   = nil;
-				reqIP.beforeFreeIP = nil;
-
-			}
-			freeIP = reqIP;
-			{//ReserveIP
-				freeIP.nextFreeIP = nil;
-				freeIP.beforeFreeIP = endReserveIP;
-
-				if endReserveIP != nil { endReserveIP.nextFreeIP = freeIP; }
-
-				endReserveIP = freeIP;
-				freeIP.stage = IP_Reserved;
-			}
+			removeLease(resL, &firstReserveL, &nilL);
+			issuedL = resL;
+			issuedL.stage = IP_Issued;
 
 			return;
 		};
-		var getReserveIP = func(resIP *queueIP) (freeIP *queueIP){
-			{//ReserveIP
-				if resIP.beforeFreeIP != nil { resIP.beforeFreeIP.nextFreeIP = resIP.nextFreeIP; }
-				if resIP.nextFreeIP   != nil { resIP.nextFreeIP.beforeFreeIP = resIP.beforeFreeIP; }
+		//var RetIssuedL = func(issuedL/*!nil*/ *linkedLease){
+		//	putLease(issuedL, &qFirstFreeL, &qLastFreeL, IP_Free);
+		//};
+		var UniRemoveLease = func(lease/*!nil*/ *linkedLease)  *linkedLease{
+			var nilL *linkedLease;
 
-				if resIP == endReserveIP { endReserveIP = resIP.beforeFreeIP; }
-
-				resIP.nextFreeIP   = nil;
-				resIP.beforeFreeIP = nil;
+			switch lease.stage {
+			case IP_Free:     removeLease(lease, &qFirstFreeL,   &qLastFreeL);
+			case IP_Reserved: removeLease(lease, &firstReserveL, &nilL      );
 			}
-			freeIP = resIP;
-
-			freeIP.stage = IP_NotFree;
-
-			return;
+			return lease;
 		};
-		var retNotFreeIP = func(freeIP *queueIP) {
-			freeIP.nextFreeIP = nil;
-			freeIP.beforeFreeIP = endFreeIP;
-
-			if endFreeIP != nil { endFreeIP.nextFreeIP = freeIP;
-			} else { lastFreeIP = freeIP; }
-
-			endFreeIP = freeIP;
-			freeIP.stage = IP_Free;
-		};
-
-		var clientMAC = map[MAC48] *queueIP {};
-
-		var safeSetMAC = func(ip *queueIP, MAC MAC48) *queueIP{
-			if clientMAC[MAC] != nil && clientMAC[MAC] != ip {
-				if ip.stage == IP_Reserved {
-					getReserveIP(ip);
-				}
-				retNotFreeIP(ip);
+		var UniPutLease = func(lease/*!nil*/ *linkedLease, stage IPStage) *linkedLease{
+			switch stage {
+			case IP_Reserved: putLease(lease, &firstReserveL, &firstReserveL, IP_Reserved);
+			case IP_Free:     putLease(lease, &qFirstFreeL,   &qLastFreeL,    IP_Free);
 			}
-			clientMAC[MAC] = ip;
-			ip.mac = MAC;
+			return lease;
+		}
 
-			return ip;
-		};
-		var setMAC = func(freeIP *queueIP, MAC MAC48) *queueIP{
-			clientMAC[MAC] = freeIP;
-			freeIP.mac = MAC;
+		var clients = map[MAC48] *linkedLease {};
 
-			return freeIP;
+		var SetMAC = func(lease/*!nil*/ *linkedLease, mac MAC48) *linkedLease{
+			clients[mac] = lease;
+			lease.mac = mac;
+
+			return lease;
 		};
-		var deleteMAC = func(MAC MAC48){
-			clientMAC[MAC].mac = MAC48{0,0,0,0,0,0};
-			delete(clientMAC, MAC);
+		var DeleteMAC = func(mac MAC48){
+			clients[mac].mac = zeroMAC;
+			delete(clients, mac);
+		};
+
+		var NewClient = func(lease *linkedLease, mac MAC48) *linkedLease{
+			if lease != nil { SetMAC(lease, mac); }
+			return lease;
+		};
+		var OldClient = func(lease *linkedLease) *linkedLease{
+			//TODO: on debug "if lease.stage == IP_Free { panic("Bug#5464985"); }"
+			if lease.stage == IP_Issued { UniPutLease(lease, IP_Reserved); }
+			return lease;
+		};
+		var ReSetClient = func(lease/*!nil*/ *linkedLease, mac MAC48, stage IPStage) *linkedLease{
+			UniPutLease(UniRemoveLease(clients[mac]), IP_Free);
+			SetMAC(UniPutLease(UniRemoveLease(lease), stage), mac);
+
+			return lease;
+		};
+		var DeleteClient = func(mac MAC48) net.IP{
+			//TODO: on debug "if clients[mac].stage == IP_Free { panic("Bug#-5464985-"); }"
+			UniPutLease(UniRemoveLease(clients[mac]), IP_Free);
+			DeleteMAC(mac);
+
+			return nil;
 		};
 
 
@@ -294,23 +295,11 @@ func main() {
 		//TODO: minimize load: lease_time += ip_offset
 		//TODO: network IO is async?
 
-		//TODO: разъеденить функции <-------------------------------------
+		//TODO: разъеденить функции <-------------------------------------VVVVVVV-----(внизу)
 
 		return dhcp.Handler{
 			ServeDHCP: func(req dhcp.Packet, msgType dhcp.MessageType, options dhcp.Options) dhcp.Packet {
-				var deleteUser = func(mac MAC48) net.IP{
-					switch clientMAC[mac].stage {
-					case IP_Reserved:
-						retNotFreeIP(getReserveIP(clientMAC[mac]));
-						deleteMAC(mac);
-					case IP_NotFree:
-						retNotFreeIP(clientMAC[mac]);
-						deleteMAC(mac);
-					case IP_Free://detect error in this program
-						panic("Bug#-5464985-");
-					}
-					return nil;
-				}
+
 				/*
 				switch android, iPhone, PC, ...
 				*/
@@ -331,47 +320,30 @@ func main() {
 						var reqMAC MAC48; copy(reqMAC[:], req.CHAddr());
 						var reqIP = net.IP(options[dhcp.OptionRequestedIPAddress]);
 
-						if tr:=req.CIAddr(); reqIP == nil && !tr.Equal(net.IP{0,0,0,0}) { reqIP = tr; }
+						if tr:=req.CIAddr(); reqIP == nil && !tr.Equal(zeroIP) { reqIP = tr; }
 
 						if reqIP!=nil { if r:=dhcp.IPRange(START_IP, reqIP)-1; !(r>=0 && r<IP_Range) { reqIP=nil; } }
 
 						var outIP net.IP;
 
-						var freeIPTestMAC = func(mac MAC48) (ip net.IP){
-							var record = clientMAC[mac];
-
-							switch record.stage {
-							case IP_Reserved://double
-								ip = record.ip;
-							case IP_NotFree://my friend
-								//change state to IP_Reserved
-								ip = getFreeIP(record).ip;
-							case IP_Free://detect error in this program
-								panic("Bug#5464985");
-							}
-							return;
-						};
-						var setIpAndMac = func(record *queueIP, mac MAC48) net.IP{
-							if record != nil { setMAC(record, mac); }
-							return record.ip;
-						};
+						var reqMACLease = clients[reqMAC];
 
 						switch {
-						case reqIP == nil && clientMAC[reqMAC] == nil: outIP = setIpAndMac(getLastFreeIP(), reqMAC);
-						case reqIP == nil && clientMAC[reqMAC] != nil: outIP = freeIPTestMAC(reqMAC);
-						case reqIP != nil && clientMAC[reqMAC] == nil:
-							var qReqIP = offsetToIP(dhcp.IPRange(START_IP, reqIP)-1);
-							switch qReqIP.stage {
-							case IP_Free:                 outIP = setIpAndMac(getFreeIP(qReqIP), reqMAC); //use user IP
-							case IP_Reserved, IP_NotFree: outIP = setIpAndMac(getLastFreeIP(),   reqMAC); //try send another ip
+						case reqIP == nil && reqMACLease == nil: outIP = NewClient(LastFreeL(), reqMAC).ip;
+						case reqIP == nil && reqMACLease != nil: outIP = OldClient(reqMACLease).ip;
+						case reqIP != nil && reqMACLease == nil:
+							var reqIPLease = Lease(dhcp.IPRange(START_IP, reqIP)-1);
+							switch reqIPLease.stage {
+							case IP_Free:                outIP = NewClient(GetFreeL(reqIPLease), reqMAC).ip; //use user IP
+							case IP_Reserved, IP_Issued: outIP = NewClient(LastFreeL(),          reqMAC).ip; //try send another lease
 							}
-						case reqIP != nil && clientMAC[reqMAC] != nil:
-							var qReqIP = offsetToIP(dhcp.IPRange(START_IP, reqIP)-1);
-							if qReqIP.mac == reqMAC {         outIP = freeIPTestMAC(reqMAC);
+						case reqIP != nil && reqMACLease != nil:
+							var reqIPLease = Lease(dhcp.IPRange(START_IP, reqIP)-1);
+							if reqIPLease.mac == reqMAC {    outIP = OldClient(reqMACLease).ip;
 							}else{
-								switch qReqIP.stage {
-								case IP_Free:                 outIP = getFreeIP(safeSetMAC(qReqIP, reqMAC)).ip; //move to user IP
-								case IP_Reserved, IP_NotFree: outIP = freeIPTestMAC(reqMAC);
+								switch reqIPLease.stage {
+								case IP_Free:                outIP = ReSetClient(reqIPLease, reqMAC, IP_Reserved).ip; //move to user IP
+								case IP_Reserved, IP_Issued: outIP = OldClient(reqMACLease).ip;
 								}
 							}
 						}
@@ -379,15 +351,15 @@ func main() {
 						if outIP != nil { return dhcp.ReplyPacket(req, dhcp.Offer, SERVER_IP, outIP, time.Hour, nil); }
 
 						return nil;
-						/*return dhcp.ReplyPacket(req, dhcp.Offer, SERVER_IP, clientMAC[reqMAC],
+						/*return dhcp.ReplyPacket(req, dhcp.Offer, SERVER_IP, clients[reqMAC],
 							h.options.SelectOrderOrAll(options[dhcp.OptionParameterRequestList]), nil);*/
 					},
 					dhcp.Request: func() dhcp.Packet{
 						var reqMAC MAC48; copy(reqMAC[:], req.CHAddr());
-						var reqIP = net.IP(options[dhcp.OptionRequestedIPAddress]);
+						var reqIP       = net.IP(options[dhcp.OptionRequestedIPAddress]);
 						var reqServerIP = net.IP(options[dhcp.OptionServerIdentifier]);
 
-						if tr:=req.CIAddr(); reqIP == nil && !tr.Equal(net.IP{0,0,0,0}) { reqIP = tr; }
+						if tr:=req.CIAddr(); reqIP == nil && !tr.Equal(zeroIP) { reqIP = tr; }
 
 						//very buggy client
 						if reqServerIP == nil && reqIP == nil { return nil; }
@@ -395,50 +367,38 @@ func main() {
 						//try correct
 						if reqServerIP == nil { reqServerIP = SERVER_IP; }
 						if reqIP == nil {
-							if reqIP = clientMAC[reqMAC].ip; reqIP == nil { return nil; }	//correct fail
+							if reqIP = clients[reqMAC].ip; reqIP == nil { return nil; }	//correct fail
 						}
 
 						var outIP net.IP;
 
-						var reqIPInRange bool; {
-							r:=dhcp.IPRange(START_IP, reqIP)-1;
-							reqIPInRange = r>=0 && r<IP_Range;
-						}
-						var serversIPEqual = reqServerIP.Equal(SERVER_IP);
+						var offsetIP = dhcp.IPRange(START_IP, reqIP)-1;
+						var reqIPInRange = offsetIP>=0 && offsetIP<IP_Range;
+						var reqMACInClients = clients[reqMAC] != nil;
 
 						switch {
-						case reqIPInRange && clientMAC[reqMAC] != nil:
-							var qReqIP = offsetToIP(dhcp.IPRange(START_IP, reqIP)-1);
-							if qReqIP.mac == reqMAC {										//TODO: test speed "offsetToIP(offsetReqIP)==clientMAC[reqMAC]" && "clientMAC[reqMAC].ip.Equal(reqIP)"
-								switch qReqIP.stage {
-								case IP_Reserved:
-									//change state to IP_NotFree
-									getReserveIP(qReqIP);
-									outIP = reqIP;
-								case IP_NotFree:
-									outIP = reqIP;
-								case IP_Free://detect error in this program
-									panic("Bug#+5464985+");
-								}
+						case reqIPInRange && reqMACInClients:
+							var reqIPLease = Lease(offsetIP);
+							if reqIPLease.mac == reqMAC {										//TODO: test speed "Lease(offsetReqIP)==clients[reqMAC]" && "clients[reqMAC].lease.Equal(reqL)"
+								if reqIPLease.stage == IP_Reserved { GetReserveL(reqIPLease); }	//TODO: on debug "if reqIPLease.stage == IP_Free { panic("Bug#+5464985+"); }"
+								outIP = reqIP;
 							}else{
-								switch qReqIP.stage {
-								case IP_Free:                 outIP = getReserveIP(getFreeIP(safeSetMAC(qReqIP, reqMAC))).ip; //move to user IP
-								case IP_Reserved, IP_NotFree: outIP = deleteUser(reqMAC);
+								switch reqIPLease.stage {
+								case IP_Free:                outIP = ReSetClient(reqIPLease, reqMAC, IP_Issued).ip; //move to user IP
+								case IP_Reserved, IP_Issued: outIP = DeleteClient(reqMAC);
 								}
 							}
-						case reqIPInRange && clientMAC[reqMAC] == nil:
-							var qReqIP = offsetToIP(dhcp.IPRange(START_IP, reqIP)-1);
-							switch qReqIP.stage {
-							case IP_Free:                 outIP = setMAC(getReserveIP(getFreeIP(qReqIP)), reqMAC).ip; //use user IP
-							case IP_Reserved, IP_NotFree: outIP = nil; //user broken off
+						case reqIPInRange && !reqMACInClients:
+							var reqIPLease = Lease(offsetIP);
+							switch reqIPLease.stage {
+							case IP_Free:                outIP = SetMAC(UniPutLease(UniRemoveLease(reqIPLease), IP_Issued), reqMAC).ip; //use user IP
+							case IP_Reserved, IP_Issued: outIP = nil; //user broken off
 							}
-						case !reqIPInRange && clientMAC[reqMAC] != nil:
-							outIP = deleteUser(reqMAC);
-						case !reqIPInRange && clientMAC[reqMAC] == nil:
-							outIP = nil;
+						case !reqIPInRange &&  reqMACInClients: outIP = DeleteClient(reqMAC);
+						case !reqIPInRange && !reqMACInClients: outIP = nil;
 						}
 
-						if serversIPEqual {
+						if reqServerIP.Equal(SERVER_IP) {
 							if outIP != nil { return dhcp.ReplyPacket(req, dhcp.ACK, SERVER_IP, outIP, time.Hour, nil);
 							}else           { return dhcp.ReplyPacket(req, dhcp.NAK, SERVER_IP, nil,   time.Hour, nil) }
 						}
@@ -448,9 +408,9 @@ func main() {
 					dhcp.Decline: func() dhcp.Packet{
 						var reqMAC MAC48; copy(reqMAC[:], req.CHAddr());
 
-						if clientMAC[reqMAC] != nil {
-							log.Println("IP conflict (Decline msg) - IP: " + clientMAC[reqMAC].ip.String() + " MAC: " + net.HardwareAddr(reqMAC[:]).String());
-							deleteUser(reqMAC);
+						if clients[reqMAC] != nil {
+							log.Println("leases conflict (Decline msg) - leases: " + clients[reqMAC].ip.String() + " MAC: " + net.HardwareAddr(reqMAC[:]).String());
+							DeleteClient(reqMAC);
 						}
 
 						return nil;
@@ -459,10 +419,10 @@ func main() {
 						var reqMAC MAC48; copy(reqMAC[:], req.CHAddr());
 						var reqIP  = req.CIAddr();
 
-						if reqIP.Equal(net.IP{0,0,0,0}) { reqIP = nil; }
+						if reqIP.Equal(zeroIP) { reqIP = nil; }
 
-						if clientMAC[reqMAC] != nil && !(reqIP != nil && !clientMAC[reqMAC].ip.Equal(reqIP)) {
-							deleteUser(reqMAC);
+						if clients[reqMAC] != nil && !(reqIP != nil && !clients[reqMAC].ip.Equal(reqIP)) {
+							DeleteClient(reqMAC);
 						}
 
 						return nil;
@@ -471,7 +431,7 @@ func main() {
 						var reqMAC MAC48; copy(reqMAC[:], req.CHAddr());
 						var reqIP  = req.CIAddr();
 
-						if clientMAC[reqMAC].ip.Equal(reqIP) { // + "clientMAC[reqMAC] != nil" :-)
+						if clients[reqMAC].ip.Equal(reqIP) { // + "clients[reqMAC] != nil" :-)
 							return dhcp.ReplyPacket(req, dhcp.ACK, SERVER_IP, nil, 0, nil);
 						}else{
 							log.Println("Interesting (Inform msg)...");
