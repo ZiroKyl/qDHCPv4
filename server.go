8d60b531abf0a4a1bae992f1fe2d940a540e93ec
ZiroKyl <searchcool@yandex.ru>
2015-01-03 16:13:11 +0600

    Fix bug: lease time = 0 on first minute

--- server.go
+++ server.go
@@ -284,7 +284,8 @@ func(H *DhcpHandler) correctICurrTLeaseEnd(tNowM int){
 	for _ = range H._T_LEASE_END {
 		// L a R -> len(a-L)+len(R-a)=len(R-L)
 		if modOneDay(tNowM-int(H._T_LEASE_END[H.iCurrTLeaseEnd])) + modOneDay(int(H._T_LEASE_END[H.NextTLeaseEnd()])-tNowM) ==
-		   modOneDay(int(H._T_LEASE_END[H.NextTLeaseEnd()]-H._T_LEASE_END[H.iCurrTLeaseEnd])) {
+		   modOneDay(int(H._T_LEASE_END[H.NextTLeaseEnd()]-H._T_LEASE_END[H.iCurrTLeaseEnd])) &&
+		   modOneDay(int(H._T_LEASE_END[H.NextTLeaseEnd()])-tNowM)!=0 {
 			break;
 		}
 		H.iCurrTLeaseEnd = H.NextTLeaseEnd();
